2ad6f0945f0efea6af4b65df3ee8b70091baaf29
Tobias Sibera <Discjoggy@users.noreply.github.com>
2015-02-20 04:27:43 +0100

    Update ramdisk_device.h

--- RAMDisk/ramdisk_device.h
+++ RAMDisk/ramdisk_device.h
@@ -1,31 +1,26 @@
 /* 
- * File:   ramdisk_device.h
- * Author: Tobias Sibera, Jens Overmoeller
- *
- * Created on 8. Januar 2015, 20:43
+ * File:        :   ramdisk_device.h
+ * Author       :   Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+ *                  Jens Overmoeller <Jens.Overmoeller@HS-Osnabrueck.de>
+ * Copyright    :   GPLv3
+ * Created on   :   08.01.2014, 20:43
+ * Description  :   Repräsentiert die RAMDisk auf dem Device.
  */
 #ifndef RAMDISK_DEVICE_H
 #define	RAMDISK_DEVICE_H
 
+// RAM-Settings
 #define KERNEL_SECTOR_SIZE  512UL
-#define LOGICAL_BLOCK_SIZE  512UL
-#define NO_SECTORS          256UL
-// TOTAL SIZE = LOGICAL_BLOCK_SIZE * NO_SECTORS = 1024 * 2048 bytes = 2048 KiB
+#define LOGICAL_BLOCK_SIZE  524288UL    // 1024 X 512 KiB = 524288 Bits
+#define NO_SECTORS          256UL       // (256 X 524288) / 1024 = 128 MB
+// TOTAL SIZE = LOGICAL_BLOCK_SIZE * NO_SECTORS = 524288 * 256 = 134217728 Byte
 
 static struct bd_device {
-    unsigned int size;
+    unsigned long long size;
     spinlock_t lock;
-    u8 *data;
+    char *data;
     struct request_queue *queue;
     struct gendisk *disk;
 } Device;
-
-static void ramdisk_dev_write(sector_t sector_off, u8 *buffer, unsigned int sectors) {
-    memcpy(Device.data + sector_off * KERNEL_SECTOR_SIZE, buffer, sectors * KERNEL_SECTOR_SIZE);
-}
-
-static void ramdisk_dev_read(sector_t sector_off, u8 *buffer, unsigned int sectors) {
-    memcpy(buffer, Device.data + sector_off * KERNEL_SECTOR_SIZE, sectors * KERNEL_SECTOR_SIZE);
-}
 
 #endif
