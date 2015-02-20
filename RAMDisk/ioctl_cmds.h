fe45c1f48b9c088093f08fa79f08cef415b43585
Tobias Sibera <Discjoggy@users.noreply.github.com>
2015-02-20 04:26:23 +0100

    Update ioctl_cmds.h

--- RAMDisk/ioctl_cmds.h
+++ RAMDisk/ioctl_cmds.h
@@ -1,16 +1,19 @@
 /* 
- * File:   ioctl_cmds.h
- * Author: Tobias Sibera, Jens Overmoeller
- *
- * Created on 8. Januar 2015, 23:11
+ * File:        :   ioctl_cmds.h
+ * Author       :   Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+ *                  Jens Overmoeller <Jens.Overmoeller@HS-Osnabrueck.de>
+ * Copyright    :   GPLv3
+ * Created on   :   08.01.2015, 23:11
+ * Description  :   IOCTL-Header, welcher für die Verwendung von IOCTLs nötig ist.
  */
-
 #ifndef IOCTL_CMDS_H
 #define	IOCTL_CMDS_H
 
 #define IOCTLCMD_MAGIC 'r'
+#define IOCTLCMD_MAXNR 1
 
 // Commands
-#define IOCTLCMD_RAM_SIZE _IOR(IOCTLCMD_MAGIC, 0, int)
+#define IOCTLCMD_RAM_PCI_VER    _IOR(IOCTLCMD_MAGIC, 0, int)
+#define IOCTLCMD_RAM_SIZE       _IOR(IOCTLCMD_MAGIC, 1, int)
 
 #endif
