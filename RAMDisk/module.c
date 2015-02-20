733f2eb4b95fcdf464ebd5c60595aefacec076fb
Tobias Sibera <Discjoggy@users.noreply.github.com>
2015-02-20 04:28:11 +0100

    Update module.c

--- RAMDisk/module.c
+++ RAMDisk/module.c
@@ -1,27 +1,36 @@
 /* 
- * File:   module.c
- * Author: Tobias Sibera, Jens Overmoeller
- *
- * Created on 07. Februrary 2015, 02:47
+ * File:        :   module.c
+ * Author       :   Tobias Sibera <Tobias.Sibera@HS-Osnabrueck.de>
+ *                  Jens Overmoeller <Jens.Overmoeller@HS-Osnabrueck.de>
+ * Copyright    :   GPLv3
+ * Created on   :   07.02.2015, 02:47
+ * Description  :   Enthält die Init- und Exit-Methoden zur (De-)Registierung des Treibers.
  */
 #include <linux/module.h>
 #include "ramdisk.h"
 
+/**
+ * Registriert den Treiber im Kernel.
+ * @return  0 falls kein Fehler aufgetreten ist, andernfalls != 0.
+ */
 static int inv_module_init(void) {
     int retval = 0;
-    printk(KERN_DEBUG "<%s, %d> | Start\n", __FUNCTION__, __LINE__);
+    DEBUG_MSG(KERN_DEBUG "<%s, %d> | Start\n", __FUNCTION__, __LINE__);
     if ((retval = ramdisk_init()) != 0) {
-        printk(KERN_DEBUG "<%s, %d> | Error in ramdisk_init\n", __FUNCTION__, __LINE__);
+        DEBUG_MSG(KERN_DEBUG "<%s, %d> | Error in ramdisk_init\n", __FUNCTION__, __LINE__);
         return retval;
     }
-    printk(KERN_DEBUG "<%s, %d> | Done\n", __FUNCTION__, __LINE__);
+    DEBUG_MSG(KERN_DEBUG "<%s, %d> | Done\n", __FUNCTION__, __LINE__);
     return 0;
 }
 
+/**
+ * Derigistriert den Treiber aus dem Kernel.
+ */
 static void inv_module_exit(void) {
-    printk(KERN_DEBUG "<%s, %d> | Start\n", __FUNCTION__, __LINE__);
+    DEBUG_MSG(KERN_DEBUG "<%s, %d> | Start\n", __FUNCTION__, __LINE__);
     ramdisk_exit();
-    printk(KERN_DEBUG "<%s, %d> | Done\n", __FUNCTION__, __LINE__);
+    DEBUG_MSG(KERN_DEBUG "<%s, %d> | Done\n", __FUNCTION__, __LINE__);
 }
 
 module_init(inv_module_init);
